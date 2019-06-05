631bdf9fec234298fa73a8a1d091d33bfdddd68d
jeremie <alcaraz.jeremie@hotmail.fr>
2019-06-05 23:39:03 +0200

    Clean POM

--- src/main/java/com/escalade/data/repository/SiteRepository.java
+++ src/main/java/com/escalade/data/repository/SiteRepository.java
@@ -3,12 +3,14 @@ package com.escalade.data.repository;
 import com.escalade.data.model.Site;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+
 import java.util.List;
 
-public interface SiteRepository extends CrudRepository<Site, Integer> {
+public interface SiteRepository extends CrudRepository<Site, Integer>, JpaSpecificationExecutor {
 
     @Query(value= "select * from site  where topo_id = ?1", nativeQuery = true)
     List<Site> findAllByTopoId(int topo_id);
@@ -18,6 +20,8 @@ public interface SiteRepository extends CrudRepository<Site, Integer> {
     List<Site> listSiteBySiteId(int siteId);
 
     Page<Site> findByLocation(String location, Pageable pageable);
+
+
 
     Site findBySiteId(int siteId);
 
